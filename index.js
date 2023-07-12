6aef2f54371161824bf14d054a2a35f487ac4628
Ronald803 <ronaldblancobalboa@gmail.com>
2023-07-11 20:43:47 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+//const uploadrouter = require('./router')
+
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+//app.use(uploadrouter)
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.listen(5000,()=>{
+    console.log('App is listening on port 5000');
+})
